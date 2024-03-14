d7ec2cd6ed63642b278b7c95ce413d0dfe5785ae
Tom <twinkle1@smail.uni-koeln.de>
2024-03-14 16:28:37 +0100

    revisions

--- code/annotation_analysis/betalain_and_flavonoid_identification.sh
+++ code/annotation_analysis/betalain_and_flavonoid_identification.sh
@@ -29,3 +29,32 @@ python /home/tom/Documents/tools/KIPEs/KIPEs3.py --baits /home/tom/Documents/too
 	--subject polished_genome_annotation/annotation/Ahypochondriacus_2.2_polished_corrected.prot.fasta \
 	--seqtype pep \
 	--cpus 6
+
+# create blast db
+mkdir data/annotation_analysis/flavonoids/blast_db
+makeblastdb -in polished_genome_annotation/assembly/Ahypochondriacus_2.2_polished.softmasked.fasta -out data/annotation_analysis/flavonoids/blast_db/db -dbtype nucl
+
+# blast searches for unidentified candidate genes using KIPEs bait sequences
+tblastn -query data/annotation_analysis/flavonoids/KIPEs/blast_query/ANS.fasta -db data/annotation_analysis/flavonoids/blast_db/db -outfmt 7 > data/annotation_analysis/flavonoids/ANS_blast.out
+tblastn -query data/annotation_analysis/flavonoids/KIPEs/blast_query/ANR.fasta -db data/annotation_analysis/flavonoids/blast_db/db -outfmt 7 > data/annotation_analysis/flavonoids/ANR_blast.out
+tblastn -query data/annotation_analysis/flavonoids/KIPEs/blast_query/F3-5-H.fasta -db data/annotation_analysis/flavonoids/blast_db/db -outfmt 7 > data/annotation_analysis/flavonoids/F3-5-H_blast.out
+tblastn -query data/annotation_analysis/flavonoids/KIPEs/blast_query/FNS1.fasta -db data/annotation_analysis/flavonoids/blast_db/db -outfmt 7 > data/annotation_analysis/flavonoids/FNS1_blast.out
+tblastn -query data/annotation_analysis/flavonoids/KIPEs/blast_query/CHI2.fasta -db data/annotation_analysis/flavonoids/blast_db/db -outfmt 7 > data/annotation_analysis/flavonoids/CHI2_blast.out
+
+# exonerate protein alignments
+# prepare fasta file of KIPEs bait sequences
+sed 's/%_//' data/annotation_analysis/flavonoids/KIPEs/blast_query/ANS.fasta > data/annotation_analysis/flavonoids/ANS_fixed.fasta
+# run exonerate
+exonerate --model protein2genome --percent 55 --showvulgar no --showalignment no --showtargetgff yes --query data/annotation_analysis/flavonoids/ANS_fixed.fasta --target polished_genome_annotation/assembly/Ahypochondriacus_2.2_polished.softmasked.fasta > data/annotation_analysis/flavonoids/ANS_exonerate.gff
+
+sed 's/%_//' data/annotation_analysis/flavonoids/KIPEs/blast_query/ANR.fasta > data/annotation_analysis/flavonoids/ANR_fixed.fasta
+exonerate --model protein2genome --percent 55 --showvulgar no --showalignment no --showtargetgff yes --query data/annotation_analysis/flavonoids/ANR_fixed.fasta --target polished_genome_annotation/assembly/Ahypochondriacus_2.2_polished.softmasked.fasta > data/annotation_analysis/flavonoids/ANR_exonerate.gff
+
+sed 's/%_//' data/annotation_analysis/flavonoids/KIPEs/blast_query/F3-5-H.fasta > data/annotation_analysis/flavonoids/F3-5-H_fixed.fasta
+exonerate --model protein2genome --percent 55 --showvulgar no --showalignment no --showtargetgff yes --query data/annotation_analysis/flavonoids/F3-5-H_fixed.fasta --target polished_genome_annotation/assembly/Ahypochondriacus_2.2_polished.softmasked.fasta > data/annotation_analysis/flavonoids/F3-5-H_exonerate.gff
+
+sed 's/%_//' data/annotation_analysis/flavonoids/KIPEs/blast_query/FNS1.fasta > data/annotation_analysis/flavonoids/FNS1_fixed.fasta
+exonerate --model protein2genome --percent 55 --showvulgar no --showalignment no --showtargetgff yes --query data/annotation_analysis/flavonoids/FNS1_fixed.fasta --target polished_genome_annotation/assembly/Ahypochondriacus_2.2_polished.softmasked.fasta > data/annotation_analysis/flavonoids/FNS1_exonerate.gff
+
+sed 's/%_//' data/annotation_analysis/flavonoids/KIPEs/blast_query/CHI2.fasta > data/annotation_analysis/flavonoids/CHI2_fixed.fasta
+exonerate --model protein2genome --percent 55 --showvulgar no --showalignment no --showtargetgff yes --query data/annotation_analysis/flavonoids/CHI2_fixed.fasta --target polished_genome_annotation/assembly/Ahypochondriacus_2.2_polished.softmasked.fasta > data/annotation_analysis/flavonoids/CHI2_exonerate.gff
---
+++ raw_data/betalain_quantification/readme.txt
@@ -0,0 +1,7 @@
+## raw_data/betalain_quantification/
+
+Betalain quantification from leaf and flower of parental lines used in the bulk segregant analysis (BSA) and of amaranth hairy roots of AhMYB2.1 overexpression.
+
+Quantification of betalain content in leaf and flower from BSA parental lines using photometry (sheet "Photometric_quantification") and LC-MS (sheet "LC-MS_quantification"). Photometric and LC-MS quantification was done from the same betalain extracts. Sample names in LC-MS quantification correspond to tube_nr column in the photometric quantification sheet. For betalain extraction, the initial solvent(H2O)/fresh weight ratio was set equal within tissue and batch. Relative quantification using LC-MS was conducted against the internal standard sinigrin.
+
+Relative quantification from from hairy roots of AhMYB2.1 overexpression plants (sheet "transgenic_roots_LC-MS_quantification"). Transformed and wild-type like tissue was sampled from two successfully transformed individuals.
